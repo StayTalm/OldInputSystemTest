a3769c70f6096d773f9ca3d776d84ffc5a3d4230
Tom Baird <tomb@unity3d.com>
2017-07-11 12:48:14 -0700

    First Commit, basic project

---
+++ Assets/InputReporter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputReporter : MonoBehaviour
+{
+    public Text textScript;
+
+    [Serializable]
+    public struct InputElement
+    {
+        public string Name;
+        public string IdName;
+    }
+
+    public List<InputElement> inputElements = new List<InputElement>();
+
+    public void Update()
+    {
+        StringBuilder sb = new StringBuilder();
+    }
+}
