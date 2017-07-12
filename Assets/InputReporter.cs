7fd91189f57906d0061adfc154dd0cabf53be3e9
Tom Baird <tomb@unity3d.com>
2017-07-11 19:02:46 -0700

    Super Simple Input Logger for a few mouse, keyboard, and gamepad events

--- Assets/InputReporter.cs
+++ Assets/InputReporter.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,14 +11,49 @@ public class InputReporter : MonoBehaviour
     [Serializable]
     public struct InputElement
     {
+		public enum ElementType
+		{
+			Axis,
+			Button
+		}
+
         public string Name;
         public string IdName;
+		public ElementType elementType;
+
     }
 
     public List<InputElement> inputElements = new List<InputElement>();
 
     public void Update()
     {
-        StringBuilder sb = new StringBuilder();
+		StringBuilder sb = new StringBuilder ("CurrentInputState:\n");
+		for (int i = 0; i < inputElements.Count; i++) 
+		{
+			InputElement element = inputElements [i];
+			sb.Append (element.Name);
+			sb.Append (" : ");
+			switch (element.elementType) 
+			{
+			case InputElement.ElementType.Axis:
+				{
+					sb.Append (Input.GetAxis (element.Name));
+				}
+				break;
+			case InputElement.ElementType.Button:
+				{
+					sb.Append(Input.GetButton(element.IdName));
+				}
+				break;
+			}
+
+			sb.Append ("\n");
+
+		}
+
+		if (textScript != null) 
+		{
+			textScript.text = sb.ToString ();
+		}
     }
 }
